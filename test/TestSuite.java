01d2d4b05cc957727d86854b29a61b3f3307dfda
Gopal S Akshintala <gopalakshintala@gmail.com>
2016-11-04 10:54:15 +0530

    Minor Changes

--- test/TestSuite.java
+++ test/TestSuite.java
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2016, Gopal S Akshintala 
- * This source code is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License.
- * http://creativecommons.org/licenses/by-sa/4.0/
- */
-
 import graph.ConnectionEdgeTest;
 import graph.FamilyGraphTest;
 import loader.FileLoaderServiceTest;
